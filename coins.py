95a0100510fa5cca8abe7c5be9517df6a54297d3
caohuajia <553059894@qq.com>
2024-10-07 00:45:08 +0800

    add coin nmr

--- coins.py
+++ coins.py
@@ -6,6 +6,9 @@ coin_property = {\
     "PEOPLE"    :{"burst"  :0.01,
                   "money_u":1\
                  },\
+    "NMR"       :{"burst"  :0.01,
+                  "money_u":1\
+                 },\
     "UXLINK"    :{"burst"  :0.01,
                   "money_u":1\
                  }\

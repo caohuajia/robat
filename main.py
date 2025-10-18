d4805ef8d4d7c57c4bbd97d36ba7a836d5cd6e2e
jiajia <caohuajia@tju.edu.cn>
2025-10-18 16:29:10 +0800

    add

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Coin():
     global cur_ctime
     global config_dict
     global prefer_idx
+    global back_test
     log = ""
     coin_name = ""
 
@@ -535,6 +536,7 @@ def create_working_order():
 if __name__ == "__main__":
     time_mode = "1m"  ## 1m or 15m
 
+    back_test = 0
     global_log = ""
 
     config_dict = get_config()
